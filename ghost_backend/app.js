3345658b5fd41ea95707c047093f9b815572e920
Shane John Paul <shanejohnpaul1998@gmail.com>
2022-06-22 21:23:54 +0530

    Added backend

---
+++ ghost_backend/app.js
@@ -0,0 +1,132 @@
+require("dotenv").config();
+const express = require("express");
+
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+const morgan = require("morgan");
+
+const knex = require("./knex");
+const app = express();
+const port = process.env.PORT;
+
+//Initialize tables in database
+async function initializeDB() {
+  try {
+    // Enable foreign key constraint
+    knex.raw("PRAGMA foreign_keys = ON;").then(() => {
+      console.log("Foreign Key Check activated.");
+    });
+
+    await knex.schema.hasTable("users").then((exists) => {
+      if (!exists) {
+        return knex.schema.createTable("users", (table) => {
+          table.increments("id").primary(); // user ID
+          table.string("name"); // user name (Display name with caps)
+        });
+      }
+    });
+    await knex.schema.hasTable("comments").then((exists) => {
+      if (!exists) {
+        return knex.schema.createTable("comments", (table) => {
+          table.increments("id").primary(); // comment ID
+          table.integer("userid"); // relation users
+          table.foreign("userid").references("id").inTable("users"); // relation users
+          table.timestamp("ts").defaultTo(knex.fn.now()); // comment timestamp
+          table.string("msg"); //comment content
+          table.integer("upvotes").defaultTo(0); //comment content
+        });
+      }
+    });
+
+    // Adding example data
+    if ((await knex("users").select("id")).length === 0) {
+      await knex("users").insert([{ name: "Shane" }, { name: "Tom" }, { name: "Mary" }, { name: "Paula" }]);
+    }
+    if ((await knex("comments").select("id")).length == 0) {
+      await knex("comments").insert({ userid: 1, msg: "Hi. This is Shane!" });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+initializeDB().then((res) => console.log("Database Initialization complete."));
+
+app.use(cors());
+
+// log events in access.log file
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" });
+app.use(morgan("combined", { stream: accessLogStream }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  res.send("Hi! I'm the api ✌");
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await knex("users");
+    res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+
+app.get("/comments", async (req, res) => {
+  try {
+    const comments = await knex("comments").join("users", "comments.userid", "users.id").orderBy("ts", "desc");
+    res.status(200).json(comments);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+
+app.post("/comment", async (req, res) => {
+  try {
+    const addComment = await knex("comments").insert({ userid: req.body.userid, msg: req.body.msg });
+    if (addComment) res.status(200).send();
+    else res.status(500).send();
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+
+// app.post("/users", upload.array("images[]", 8), async (req, res) => {
+//   try {
+//     if (req.body.name === "" || req.body.email === "") return res.status(400).send("Name, email required");
+//     const users = await pool.query("INSERT INTO users (name, email, images) VALUES ($1, $2, $3)", [
+//       req.body.name,
+//       req.body.email,
+//       req.files.map((obj) => obj.filename), // save filenames
+//     ]);
+//     //Add embedding of each image with name and filename in another table
+//     //Create python server with the face embedding model - takes image input, gives embeddings
+//     return res.status(200).send();
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).send(err);
+//   }
+// });
+
+// app.post("/addimages", upload.array("images[]", 8), async (req, res) => {
+//   try {
+//   } catch (error) {
+//     console.log(err);
+//     res.status(500).send(err);
+//   }
+// });
+
+// catch 404 and forward to error handler
+app.use((req, res) => {
+  res.status(404).send("Page not found");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
